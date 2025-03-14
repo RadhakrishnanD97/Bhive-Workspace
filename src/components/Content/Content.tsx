2d74e38691314cbeac60ed4b7f599b1e0cfb7cd1
Radhakrishnan D <radhakrishnand97@gmail.com>
2025-03-14 22:45:59 +0530

    content mobile css added

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -10,9 +10,12 @@ function Content() {
                 </h1>
             </div>
             <div className='video-wrapper'>
-                <video className='content-video' src='/assets/content-video.mp4' autoPlay loop muted/>
+                <video className='content-video' src='/assets/content-video.mp4' autoPlay loop muted />
                 <img src='/assets/diagnol.png' className='diagnol-image' />
             </div>
+            <h1 className='mobile-title'>
+                Host your meeting with world-class amenities. Starting at <span>₹199/-!</span>
+            </h1>
         </section>
     );
 }
