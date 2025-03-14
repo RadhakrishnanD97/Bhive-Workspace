d9ccedcfee73c5530245bc0ccb96def602ab7e12
Radhakrishnan D <radhakrishnand97@gmail.com>
2025-03-14 11:09:47 +0530

    header section added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-// import logo from './logo.svg';
+import logo from './assets/logo.png';
+import phone from './assets/phone.png';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>sndfndsfnsdkj</p>
+        <img src={logo} className="bhive-logo" alt="Bhive Logo" />
+        <div className='phone-wrapper'>
+          <img src={phone} className="phone" alt="phone" />
+        </div>
       </header>
     </div>
   );
